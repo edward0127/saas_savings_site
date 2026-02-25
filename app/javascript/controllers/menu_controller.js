7034bcb035d68c1c35a25c34cc3c7b6394bfd113
link <lin.ke@woven.com.au>
2026-02-25 20:25:07 +1100

    minor change

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,11 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["panel", "openIcon", "closeIcon"]
+  static targets = ["panel", "openIcon", "closeIcon", "button"]
+
+  connect() {
+    this.syncAria()
+  }
 
   toggle() {
     this.panelTarget.classList.toggle("hidden")
     this.openIconTarget.classList.toggle("hidden")
     this.closeIconTarget.classList.toggle("hidden")
+    this.syncAria()
+  }
+
+  syncAria() {
+    if (!this.hasButtonTarget) return
+
+    this.buttonTarget.setAttribute("aria-expanded", this.panelTarget.classList.contains("hidden") ? "false" : "true")
   }
 }
